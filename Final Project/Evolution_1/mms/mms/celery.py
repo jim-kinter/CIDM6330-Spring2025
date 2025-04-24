caaa2466efbebaca8a7910847d885b2ccdb9b79f
Jim Kinter <jkinter@gmail.com>
2025-04-24 17:56:05 -0500

    Publish Evolution 1 including django and new UML drawings in the artifacts folder.

---
+++ Final Project/Evolution_1/mms/mms/celery.py
@@ -0,0 +1,9 @@
+# mms/celery.py
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mms.settings')
+
+app = Celery('mms')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
